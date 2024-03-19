2cef5b32200de8a820461f9c0c8c498dc78e20bd
linzhiji <linzhiji@gmail.com>
2024-03-19 15:38:50 +0800

    fix bug

---
+++ utilMergeFaceFromModel.py
@@ -0,0 +1,31 @@
+
+# https://www.modelscope.cn/models/iic/cv_unet_face_fusion_torch/summary
+
+import cv2
+# from modelscope.outputs import OutputKeys
+from modelscope.pipelines import pipeline
+# from modelscope.utils.constant import Tasks
+import platform
+import torch
+from utils.logger_settings import api_logger
+
+
+# exit(0)
+def mergeFace(template_path, user_path, outPath):
+    image_face_fusion = pipeline('face_fusion_torch',
+                                model='damo/cv_unet_face_fusion_torch', 
+                                model_revision='v1.0.3',
+                                device="cuda:0")
+
+    # if platform.system() == "linux":
+    #     template_path = 'https://modelscope.oss-cn-beijing.aliyuncs.com/test/images/facefusion_template.jpg'
+    #     user_path = 'https://modelscope.oss-cn-beijing.aliyuncs.com/test/images/facefusion_user.jpg'
+    # else:
+    #     template_path = './images/liudehua.jpg'
+    #     user_path = './images/zhoujielun.jpg'
+
+    print(template_path)
+    result = image_face_fusion(dict(template=template_path, user=user_path))
+
+    cv2.imwrite(outPath, result['output_img'])
+    api_logger.info('finished!')
