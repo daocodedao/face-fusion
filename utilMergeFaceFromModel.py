59929147cea81817a2306929f08d97424fffac08
linzhiji <linzhiji@gmail.com>
2024-03-19 16:30:52 +0800

    fix bug

--- utilMergeFaceFromModel.py
+++ utilMergeFaceFromModel.py
@@ -5,6 +5,7 @@ import cv2
 from modelscope.pipelines import pipeline
 import torch
 from utils.logger_settings import api_logger
+import argparse
 
 device = "cuda:0"
 if torch.cuda.device_count() > 0:
@@ -23,3 +24,22 @@ def mergeFace(template_path, user_path, outPath):
 
     cv2.imwrite(outPath, result['output_img'])
     api_logger.info('finished!')
+
+
+
+
+if(__name__ == '__main__'):
+    program = argparse.ArgumentParser(formatter_class=lambda prog: argparse.HelpFormatter(prog, max_help_position=100))
+    program.add_argument('-t', '--templatePath', help='templatePath',
+                        dest='templatePath', type=str)
+    program.add_argument('-s', '--srcPath', help='srcPath',
+                        dest='srcPath', type=str)
+    program.add_argument('-o', '--outPath', help='outPath',
+                        dest='outPath', type=str)
+
+
+    args = program.parse_args()
+
+    templatePath = args.templatePath
+    srcPath = args.srcPath
+    outPath = args.outPath  
